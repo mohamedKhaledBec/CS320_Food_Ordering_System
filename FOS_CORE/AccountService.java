571d9d74f656a63a37ab4ab1221a3d8bc6abb758
Mohamed khaled Becetti <mohamed.becetti@ozu.edu.tr>
2025-11-29 11:32:19 +0300

    work scope declaration (comment above function)

--- FOS_CORE/AccountService.java
+++ FOS_CORE/AccountService.java
@@ -6,7 +6,7 @@ public class AccountService implements IAccountService {
 
     private final DatabaseAPI DB = new DatabaseAPI();
     static int userID = 0;
-    // Working on it : Mohamed Khaled Becetti  )
+    // Working on it : Mohamed Khaled Becetti
     @Override
     public Customer createCustomerAccount(String email, String phone, String password) {
         User user = new Customer(userID, email, password);
@@ -14,34 +14,35 @@ public class AccountService implements IAccountService {
         DB.addPhoneNumber((Customer)user,phone);
         return null;
     }
-
+    //Working on it : Mohamed Khaled Becetti
     @Override
     public void changePassword(User user, String newPassword) {
         // TODO: Implementation
-    }
 
+    }
+    //Working on it : Mohamed Khaled Becetti
     @Override
     public void updateContactInfo(Customer customer, String phone) {
         // TODO: Implementation
     }
-
+    //Working on it : Mohamed Khaled Becetti
     @Override
     public boolean addAddress(Customer customer, Address address) {
         // TODO: Implementation
         return true;
     }
-
+    //Working on it : Mohamed Khaled Becetti
     @Override
     public List<Address> getAddresses(Customer customer) {
         // TODO: Implementation
         return null;
     }
-
+    //Working on it : Mohamed Khaled Becetti
     private boolean validateEmailFormat(String email) {
         // TODO: Implementation
         return false;
     }
-
+    //Working on it : Mohamed Khaled Becetti
     private void saveCustomer(Customer customer) {
         // TODO: Implementation
     }
