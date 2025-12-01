24faa0c52307a0aebc99dfed138c2316605b24c2
Umair Ahmad <umair.ahmad@ozu.edu.tr>
2025-12-01 21:42:19 +0300

    Worked on DialogUtils.java

--- FOS_UI/DialogUtils.java
+++ FOS_UI/DialogUtils.java
@@ -1,13 +1,27 @@
 package FOS_UI;
+import javax.swing.*;
+import java.awt.*;
 // Being done by Umair Ahmad
-public class DialogUtils {
+public final class DialogUtils {
     private DialogUtils(){}
 
-    public static void showError(){}
+    public static void showError(Component parent, String message){
+        JOptionPane.showMessageDialog(parent,message,"Error",JOptionPane.ERROR_MESSAGE);
+    }
 
-    public static void showInfo(){}
+    public static void showInfo(Component parent, String message){
+        JOptionPane.showMessageDialog(parent,message,"Info", JOptionPane.INFORMATION_MESSAGE);
+    }
 
-    public static boolean confirm(){return false;}
+    public static int confirm(Component parent, String message){
+        return JOptionPane.showConfirmDialog(
+                parent,
+                message,
+                "Confirm",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+        );
+    }
 
 
 
