41ac2e53e5cdda87f06d07f46fb81546c938e4fa
Umair Ahmad <umair.ahmad@ozu.edu.tr>
2025-12-02 22:44:42 +0300

    Worked on CustomerDashboardWindow.java

--- FOS_UI/CustomerDashboardWindow.java
+++ FOS_UI/CustomerDashboardWindow.java
@@ -1,4 +1,16 @@
 package FOS_UI;
+import javax.swing.*;
+import java.awt.*;
+// Being Done by Umair Ahmad
+public class CustomerDashboardWindow extends JFrame {
 
-public class CustomerDashboardWindow {
+    public CustomerDashboardWindow(){
+        setTitle("Food Ordering System - Customer Dashboard");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel label = new JLabel("Customer Dashboard", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+    }
 }
