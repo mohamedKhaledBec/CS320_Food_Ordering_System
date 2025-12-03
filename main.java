98d7870a29a384a9baedc66cf5d1b82779492b04
Umair Ahmad <umair.ahmad@ozu.edu.tr>
2025-12-03 11:57:37 +0300

    Tried running LoginWindow design.

--- main.java
+++ main.java
@@ -1,7 +1,10 @@
+import FOS_UI.LoginWindow;
 
+import javax.swing.*;
 
 public class main {
         public static void main(String[] args) {
+            SwingUtilities.invokeLater(()->{new LoginWindow().setVisible(true);} );
             System.out.println("Hello World!");
         }
 }
