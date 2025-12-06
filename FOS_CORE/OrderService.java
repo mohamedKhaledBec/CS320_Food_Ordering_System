9cc22c176034d0406879405a6ec7b4b1062d6008
Mohamed khaled Becetti <mohamed.becetti@ozu.edu.tr>
2025-12-06 16:09:43 +0300

    2 functions implemented

--- FOS_CORE/OrderService.java
+++ FOS_CORE/OrderService.java
@@ -1,18 +1,24 @@
 package FOS_CORE;
 
+import FOS_DATA.CustomerService;
+import FOS_DATA.ICustomerService;
 import java.util.ArrayList;
 
 public class OrderService implements IOrderService {
-
+    final ICustomerService DB = new CustomerService();
     @Override
-    public Order placeOrder(Customer customer, ArrayList<CartItem> cart, Address address) {
-        // TODO: Implementation
-        return null;
+    public Order placeOrder(Customer customer, ArrayList<CartItem> cart, Address address,Restaurant restaurant) {
+        Order order = new Order(address.getAddressLine(),cart,restaurant.getRestaurantName());
+        DB.insertCustomerOrder(customer,address,order,restaurant);
+        return order;
     }
 
     @Override
-    public Order trackOrder(String orderID) {
-        // TODO: Implementation
+    public Order trackOrder(int orderID, Customer customer) {
+        ArrayList<Order> orders=DB.fetchCustomerOrders(customer);
+        for (Order order : orders) {
+            if (order.getOrderID() == orderID)return order;
+        }
         return null;
     }
 
