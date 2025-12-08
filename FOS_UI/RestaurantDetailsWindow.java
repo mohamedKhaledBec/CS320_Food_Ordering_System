2312f37face2c5b82d0fc3898f086022eca97a73
Umair Ahmad <umair.ahmad@ozu.edu.tr>
2025-12-08 17:12:35 +0300

    RestaurantDetailsWindow.java (initial commit)

--- FOS_UI/RestaurantDetailsWindow.java
+++ FOS_UI/RestaurantDetailsWindow.java
@@ -1,4 +1,20 @@
 package FOS_UI;
 
-public class RestaurantDetailsWindow {
+import FOS_CORE.Customer;
+import FOS_CORE.ICartService;
+import FOS_CORE.IRestaurantService;
+import FOS_CORE.MenuItem;
+import FOS_CORE.Restaurant;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//Worked on by Umair Ahmad
+public class RestaurantDetailsWindow extends JFrame{
+
+
+
 }
