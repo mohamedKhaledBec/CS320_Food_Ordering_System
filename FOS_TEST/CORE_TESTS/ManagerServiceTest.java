99ae1fc377f005ec1885767e13d7111d48475169
Mohamed khaled Becetti <mohamed.becetti@ozu.edu.tr>
2025-12-13 12:52:28 +0300

    Ownership declaration

--- FOS_TEST/CORE_TESTS/ManagerServiceTest.java
+++ FOS_TEST/CORE_TESTS/ManagerServiceTest.java
@@ -1,3 +1,7 @@
+/**
+ * This test class is written by Mohamed Khaled Becetti
+ */
+
 package FOS_TEST.CORE_TESTS;
 
 public class ManagerServiceTest {
--- FOS_TEST/CORE_TESTS/MenuItemTest.java
+++ FOS_TEST/CORE_TESTS/MenuItemTest.java
@@ -1,4 +1,12 @@
+/**
+ * This test class is written by Mohamed Khaled Becetti
+ */
+
 package FOS_TEST.CORE_TESTS;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class MenuItemTest {
 }
